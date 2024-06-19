5a03ae32ee8ca59ff6f5d4bc09639ec22c047325
mffriaslorite <mffriaslorite@correo.ugr.es>
2024-06-19 15:12:14 +0200

    Folders and files created

---
+++ frontend/noteworks/src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+    // Your component logic here
+
+    return (
+        // Your JSX code here
+        <div>
+            <h1>Login Page</h1>
+            {/* Add your login form here */}
+        </div>
+    );
+};
+
+export default Login;
---
+++ frontend/noteworks/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SignUp = () => {
+    // Your component code here
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            {/* Add your sign up form or content here */}
+        </div>
+    );
+};
+
+export default SignUp;
