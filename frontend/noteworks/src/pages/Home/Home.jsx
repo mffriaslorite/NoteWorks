3e63a75abd6295b5f70b0a246c376e6178911d22
Robert <>
2024-06-29 01:57:16 +0200

    added in utils axios.instance and added code to frontend-pages

--- frontend/noteworks/src/pages/Home/Home.jsx
+++ frontend/noteworks/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
     return (
@@ -8,5 +9,34 @@ const Home = () => {
         </div>
     );
 };
+
+const [userInfo, setUserInfo] = useState(null);
+
+const navigate = useNavigate();
+
+//Get User Info
+const getUserInfo = async () => {
+    try {
+        const response = await axiosInstance.get("/get-user");
+        if (response.data && response.data.user) {
+            setUserInfo(response.data.user);
+        }
+    } catch (error){
+        if (error.response.status === 401) {
+            localStorage.clear();
+            navigate("/login");
+        }
+    }
+};
+
+useEffect(() => {
+    getUserInfo();
+    return () => {};
+}, []);
+
+//     return (
+//         <>
+//         <Navbar userInfo={userInfo} />
+//     )
 
 export default Home;
--- frontend/noteworks/src/pages/Login/Login.jsx
+++ frontend/noteworks/src/pages/Login/Login.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import axiosInstance from '../../utils/axios.instance';
+import {Link, useNavigate} from "react-router-dom";
+
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
     // Your component logic here
 
     return(
@@ -14,3 +23,26 @@ const Login = () => {
 };
 
 export default Login;
+
+
+//Login API Call
+try{
+    const response = await axiosInstance.post("/login", {
+        email: email,
+        password: password,
+    });
+
+//Handle succesful login response
+if (response.data && response.data.accessToken) {
+    localStorage.setItem("token", response.data.accessToken);
+    navigate("/dashboard");
+
+}
+//Handle Login error
+} catch (error){
+    if (error.response && error.response.data && error.response.data.message){
+        setError(error.response.data.message);
+    } else {
+        setError("An unexpected error occurred. Please try again.");
+    }
+};
