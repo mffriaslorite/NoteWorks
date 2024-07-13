9afa9a01aeb0aa493d505eb72a2336ce83ffa9c5
mffriaslorite <mffriaslorite@correo.ugr.es>
2024-07-13 19:00:30 +0200

    Deploy full stack application

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// API routes
+app.use('/api', require('./backend'));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
